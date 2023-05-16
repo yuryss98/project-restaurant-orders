d5122942ff1c12108e8c8dc768730ae92b2895eb
Yury Stolarz <yurysscareer@gmail.com>
2023-05-16 11:06:42 -0300

    feat: add req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,15 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ovo = Ingredient("ovo")
+    assert ovo.name == "ovo"
+    assert ovo.__repr__() == "Ingredient('ovo')"
+    assert hash(ovo) == hash(Ingredient("ovo"))
+
+    farinha = Ingredient("farinha")
+    assert farinha == Ingredient('farinha')
+    assert farinha != ovo
+    assert hash(farinha) != hash(ovo)
+    assert farinha.restrictions == {Restriction.GLUTEN}
